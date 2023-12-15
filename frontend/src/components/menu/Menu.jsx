04d31098c08e0b5b88864ba923f0d5125e65ce37
Ankit BagheL <ak9664773@gmail.com>
2023-12-15 16:20:06 +0530

    made navbar responsive

--- frontend/src/components/menu/Menu.jsx
+++ frontend/src/components/menu/Menu.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react"
+import { FaBars } from "react-icons/fa6"
+
+import { IoClose } from "react-icons/io5"
 
 import { useLocation, useNavigate } from "react-router-dom"
 import "./style.css"
@@ -7,17 +10,20 @@ function Menu() {
     const navigate = useNavigate();
     const location = useLocation();
     const [menuShow, setmenuShow] = useState(false)
+    const [show, setShow] = useState(true);
+
 
     const routing = (para) => {
         navigate(para);
-        // setShow(true)
-        // setPopUp(false)
+    }
+
+    const toggleBar = () => {
+        setShow(!show);
     }
 
     const scrollHandle = () => {
-        console.log(window.scrollY);
-        window.scrollY >= 300 ? setmenuShow(true) : setmenuShow(false)
-        console.log(menuShow);
+        window.scrollY >= 200 ? setmenuShow(true) : "";
+        window.scrollY < 30 ? setmenuShow(false) : "";
     }
 
     useEffect(() => {
@@ -34,14 +40,22 @@ function Menu() {
             <div className="menuLogo">
                 <img className='menuLogoImg' src="https://www.valuecoders.com/wp-content/themes/valuecoders/images/logo-v2.svg" alt="" />
             </div>
-            <div className="menuItems">
+
+            <div className={`menuItems ${show ? 'menuItemsMobile' : ""}`}>
                 <div className={`menuItem hoverUnderline ${location.pathname === "/" ? "active" : ""}`} onClick={() => { routing("/") }}>Home</div>
                 <div className={`menuItem hoverUnderline ${location.pathname === "/" ? "" : ""}`} onClick={() => { routing("/") }}>Services</div>
                 <div className={`menuItem hoverUnderline ${location.pathname === "/" ? "" : ""}`} onClick={() => { routing("/") }}>Services</div>
                 <div className={`menuItem hoverUnderline ${location.pathname === "/" ? "" : ""}`} onClick={() => { routing("/") }}>Services</div>
                 <div className={`menuItem hoverUnderline ${location.pathname === "/" ? "" : ""}`} onClick={() => { routing("/") }}>Services</div>
-
+                <IoClose className=" menuBar menuCross" onClick={toggleBar} />
             </div>
+
+
+
+            <FaBars className="menuBar" onClick={toggleBar} />
+
+
+
         </div>
     )
 }
